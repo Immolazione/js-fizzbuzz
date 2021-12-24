920a3128e4ff71dff470be4dfff77a46ab60de8a
Immolazione <federico.deered@gmail.com>
2021-12-24 11:27:24 +0100

    add excercise

--- js/style.js
+++ js/style.js
@@ -1,7 +1,23 @@
 console.log('JS OK');
 
+/*stampa i numeri da 1 a 100, divisibili per 3 = console.log('fizz'), 
+divisibili per 5 = console.log('buzz'), 
+divisibili per entrambi = console.log('fizzbuzz') */
+
+
 
 //stampa i numeri da 1 a 100
-//divisibile per tre = fizz
-//divisibile per cinque = buzz
-//divisibile per tre && cinque = fizzbuzz
+for (let i = 1; i <= 100; i++) {
+    //divisibile per tre && cinque = fizzbuzz
+    if ((i % 5 == 0) && (i % 3 == 0)) {
+        console.log('fizzbuzz');
+        //divisibile per cinque = buzz
+    } else if  (i % 5 == 0) {
+        console.log('buzz');
+        //divisibile per tre = fizz
+    } else if (i % 3 == 0) {
+        console.log('fizz')
+    } else {
+        console.log(i);
+    }
+}
